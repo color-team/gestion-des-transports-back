066ea7d6f36409b637070fe8b020ff755f961d1a
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-08-28 17:36:43 +0200

    Merge with master

--- scriptDocker.sh
+++
@@ -1,5 +0,0 @@
-echo "### Générer image transports-db ###"
-docker build -t postgresql-transports-db transports-db/.
-
-echo "### Créer  containeur transports-db ###"
-docker run -d --name postgresql-transports-db postgresql-transports-db
