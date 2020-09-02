855d53fb2aff6896e9aeb2bef42028eab9498ae2
Alexis PRODHOMME <alexis.prodhom@hotmail.fr>
2020-09-02 10:44:40 +0200

    USGDT008 Collab PublierAnnonce, add dto classes

--- src/main/java/dev/controller/ReservationCovoiturageController.java
+++ src/main/java/dev/controller/ReservationCovoiturageController.java
@@ -2,16 +2,20 @@ package dev.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import dev.controller.dto.AnnonceCovoiturageDto;
+import dev.controller.dto.CodeErreur;
+import dev.controller.dto.MessageErreurDto;
 import dev.controller.dto.ReservationCovoiturageDto;
 import dev.domain.ReservationCovoiturage;
+import dev.exception.ReservationCovoiturageInvalideException;
 import dev.service.ReservationCovoiturageService;
 
 @RestController
@@ -44,15 +48,15 @@ public class ReservationCovoiturageController {
 	}
 
 	// POST /reservations-covoiturage
-//	@PostMapping
-//	public ResponseEntity<?> create(@RequestBody @Valid ReservationCovoiturageDto reservationCovoiturageDto,
-//			BindingResult result) {
-//		if (result.hasErrors()) {
-//			throw new ReservationCovoiturageInvalideException(
-//					new MessageErreurDto(CodeErreur.VALIDATION, "Les champs doivent être tous remplis"));
-//		}
-//
-//		return ResponseEntity.status(HttpStatus.OK).body(service.create(reservationCovoiturageDto));
-//	}
+	/*@PostMapping
+	public ResponseEntity<?> create(@RequestBody @Valid AnnonceCovoiturageDto annonceCovoiturageDto,
+			BindingResult result) {
+		if (result.hasErrors()) {
+			throw new ReservationCovoiturageInvalideException(
+					new MessageErreurDto(CodeErreur.VALIDATION, "Les champs doivent être tous remplis"));
+		}
+
+		return ResponseEntity.status(HttpStatus.OK).body(service.create(annonceCovoiturageDto));
+	}*/
 
 }
--- src/main/java/dev/controller/mapper/ReservationCovoiturageMapper.java
+++ src/main/java/dev/controller/mapper/ReservationCovoiturageMapper.java
@@ -1,15 +1,54 @@
 package dev.controller.mapper;
 
+import dev.controller.dto.AnnonceCovoiturageDto;
 import dev.controller.dto.ReservationCovoiturageDto;
+import dev.controller.dto.VehiculeParticulierDto;
+import dev.domain.Localisation;
 import dev.domain.ReservationCovoiturage;
+import dev.domain.StatutReservationCovoiturage;
+import dev.domain.Utilisateur;
+import dev.domain.VehiculeParticulier;
 
 public class ReservationCovoiturageMapper {
+	
+	
 
 	public static ReservationCovoiturageDto toDto(ReservationCovoiturage reservationCovoiturage) {
-		return new ReservationCovoiturageDto(reservationCovoiturage.getDateDepart(),
-				reservationCovoiturage.getDepart().getAdresse(), reservationCovoiturage.getDestination().getAdresse(),
+		return new ReservationCovoiturageDto(
+				reservationCovoiturage.getDateDepart(),
+				reservationCovoiturage.getDepart().getAdresse(), 
+				reservationCovoiturage.getDestination().getAdresse(),
 				reservationCovoiturage.getStatutReservationCovoiturage().getStatutReservationCovoiturage().name(),
 				reservationCovoiturage.getVehiculeParticulier().getInfos(),
 				reservationCovoiturage.getConducteur().getInfos());
 	}
+	
+	/*public static ReservationCovoiturage fromDto( AnnonceCovoiturageDto annonce) {
+		
+		Localisation depart = new Localisation();
+		depart.setAdresse( annonce.getDepart());
+		
+		Localisation destination = new Localisation();
+		destination.setAdresse( annonce.getDestination());
+		
+		VehiculeParticulier vehicule = new VehiculeParticulier(
+				annonce.getVehicule().getImmatriculation(),
+				annonce.getVehicule().getMarque(),
+				null,
+				null,
+				annonce.getVehicule().getModele(),
+				null);
+		
+		Utilisateur conduteur = repo.findUser();
+		
+		return new ReservationCovoiturage( 
+				annonce.getDateDepart(),
+				depart, 
+				destination,
+				annonce.getVehicule().getPlaces(),
+				null,
+				vehicule,
+				'',
+				null);
+	}*/
 }
